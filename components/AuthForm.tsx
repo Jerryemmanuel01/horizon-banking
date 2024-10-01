fbb4a4e3cea65d4184e13ea97b0283341a36f9c0
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-10-01 20:41:22 +0100

    appwrite feature introduced

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,6 +14,7 @@ import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { signIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -40,15 +41,15 @@ const AuthForm = ({ type }: { type: string }) => {
       //sign up with Appwrite & create plaid token
 
       if (type === "sign-up") {
-        // const newUser = await signUp(data)
-        // setUser(newUser);
+        const newUser = await signUp(data)
+        setUser(newUser);
       }
 
       if (type === "Sign-in") {
-        // const response = await SignIn({
-        //     email:data.email,
-        //     password:data.password
-        // })
+        const response = await signIn({
+            email:data.email,
+            password:data.password
+        })
       }
     } catch (error) {
       console.log(error);
