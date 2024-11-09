01c23529ee119fbb3e94e0f3b833f6076fa48cd8
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-11-09 23:14:21 +0100

    Transaction table and table modification implement

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,11 +7,10 @@ import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
-
   const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
   const accounts = await getAccounts({
-    userId: loggedIn.$id,
+    userId: loggedIn?.$id,
   });
 
   if (!accounts) return;
