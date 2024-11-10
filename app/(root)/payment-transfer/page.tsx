85f895a94dc305957ba9cf84ae963f95d9ded5ee
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-11-10 02:46:18 +0100

    payment transform form added

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import PaymentTransferForm from "@/components/PaymentTransferForm";
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import React from "react";
 
-const Transfer = () => {
+const Transfer = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn?.$id,
+  });
+
+  if (!accounts) return;
+
+  const accountData = accounts?.data;
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="payment-transfer">
+      <HeaderBox
+        title="Payments Transfer"
+        subtext="Please provide any specific details or notes related to the payment transfer"
+      />
 
-export default Transfer
+      <section className="size-full pt-5">
+        <PaymentTransferForm accounts={accountData} />
+      </section>
+    </section>
+  );
+};
+
+export default Transfer;
