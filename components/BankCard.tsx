a3291a7894b2279ed6bb278f2dc063892957c28f
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-11-11 09:25:43 +0100

    server side pagination added on transaction tables

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -14,7 +14,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
       >
         <div className="bank-card_content">
           <div className="">
-            <h1 className="text-16 font-semi-bold text-white">{userName}</h1>
+            <h1 className="text-16 font-semi-bold text-white">{account?.name}</h1>
             <p className="font-ibm-plex-serif font-black text-white">
               {formatAmount(account.currentBalance)}
             </p>
