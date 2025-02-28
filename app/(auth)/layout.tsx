330fa3f9a7533ac4b551f27617172be4946d491a
jerryemmanuel <jerryemmanuelolisa@gmail.com>
2025-02-28 08:47:59 +0100

    fix auth bg changed

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,9 +8,16 @@ export default function RootLayout({
   return (
     <main className="flex min-h-screen w-full justify-between font-inter">
       {children}
-      <div className="auth-asset">
+      <div className="auth-asset relative">
+        <div className="absolute w-full h-full left-0 bg-black-2 opacity-60 "></div>
         <div className="">
-          <Image src="/icons/auth-image.svg" alt="image" width={500} height={500}/>
+          <Image
+            src="/icons/hero-bg1.jpg"
+            alt="image"
+            width={1500}
+            height={1500}
+            className="w-full object-cover !h-screen "
+          />
         </div>
       </div>
     </main>
